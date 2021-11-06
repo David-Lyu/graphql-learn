93e948bab9b338d94872dfbeb5370622b8b930b1
david <lyu.david@yahoo.com>
2021-11-05 17:41:10 -0700

    used apollo server to graphql

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+
 const app = express();
 
 // app.use('/graphql');
-const User = {
-  name: string
-};
+// const User = {
+//   name: string
+// };
 
 const schema = buildSchema(`
   type Query {
     hello: String,
-    stuff: User
   },
-`);
+  `);
 
 const root = {
   hello: () => {
     return 'Hello World!';
-  },
-  stuff: () => {
-    return {
-      name: 'David'
-    };
   }
 };
 
@@ -36,3 +31,40 @@ app.use(
 );
 
 app.listen(5000, () => console.log('Server is running'));
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+const { ApolloServer, gql } = require('apollo-server');
+const lifts = require('./data/lifts.json');
+const trails = require('./data/trails.json');
+const url = 'localhost:4000';
+
+const typeDefs = gql`
+  type Lift {
+    id: ID!
+    name: String!
+    capacity: Int!
+    status: LiftStatus!
+  }
+  enum LiftStatus {
+    OPEN
+    HOLD
+    CLOSED
+  }
+  type Query {
+    liftCount: Int!
+    allLifts: [Lift!]!
+  }
+`;
+
+//can call rest api and query db etc
+const resolvers = {
+  Query: {
+    liftCount: () => {
+      return lifts.length;
+    },
+    allLifts: () => lifts
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log('Server running at ' + url));
